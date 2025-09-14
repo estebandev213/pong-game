68e5dd70bbdaec8bed06a00543c76f70d8fdd3a6
Esteban Villagarcia <estebanvc.dev@gmail.com>
2025-09-13 20:11:08 -0500

    Compiler configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,20 @@
 #include "raylib.h"
 
 int main() {
-    // Crear ventana
-    InitWindow(800, 600, "Pong: The Game");
 
-    // FPS deseados
+    InitWindow(800, 600, "Pong: The Game");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        // Lógica del juego (vacía por ahora)
 
-        // Dibujado
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Aquí escribimos en pantalla
         DrawText("Hello World!", 300, 280, 30, BLACK);
 
         EndDrawing();
     }
 
-    CloseWindow(); // Cerrar ventana
+    CloseWindow();
     return 0;
 }
